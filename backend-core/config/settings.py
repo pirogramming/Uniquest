abb00471c99d0a6d78a7db99084fd24b41d0d414
chanwoong <cwyoon9718@gmail.com>
2026-01-27 14:32:02 +0900

    docs:settings.py 에 메일 설정을 위한 env 변수들 추가, urls에 user경로 추가

--- backend-core/config/settings.py
+++ backend-core/config/settings.py
@@ -12,9 +12,29 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# 1. 환경 변수를 읽어올 객체 생성
+env = environ.Env(
+    DEBUG=(bool, False) # 기본값 설정
+)
+
+# 2. .env 파일을 읽어들임
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# 3. 값 할당
+DEBUG = env('DEBUG')
+
+# 메일 설정에 활용
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 
 # Quick-start development settings - unsuitable for production
