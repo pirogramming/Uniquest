5263ebf3facbb3b69a56edde1d2cd087997a53c9
chanwoong <cwyoon9718@gmail.com>
2026-01-27 22:26:11 +0900

    docs:로그인관련해서 urls.py path 변경

--- backend-core/users/urls.py
+++ backend-core/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RegisterView, ProfileView, UnivCertView
+from .views import RegisterView, ProfileView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, # 로그인 (Access + Refresh 토큰 발급)
     TokenRefreshView,    # 토큰 갱신
 )
 from . import views
+from .views import MyLoginView
 
 app_name = 'users'
 
@@ -14,9 +15,10 @@ urlpatterns = [
     path('verify-email/', views.verify_email, name='verify-email'),
     
     # 로그인 (이거 하나면 끝!)
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/',views.login_page,name="login"),
+    path('login_logic/', MyLoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('verify-univ/', UnivCertView.as_view(), name='verify_univ'),
+    # path('verify-univ/', UnivCertView.as_view(), name='verify_univ'),
 ]
