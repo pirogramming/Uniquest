4e1cc2044b60fce456c69dccbdc037dc5f49823f
Jonghyun <a1234@Jonghyun--MacBook-Pro.local>
2026-01-29 03:22:42 +0900

    chore: fixed merged error btw develop&feat-jh

--- backend-core/users/views.py
+++ backend-core/users/views.py
@@ -125,46 +125,6 @@ class ProfileView(views.APIView):
         serializer = UserProfileSerializer(request.user)
         return Response(serializer.data)
 
-<<<<<<< HEAD
-# # 3. 대학생 인증 처리 View (Univcert 연동)
-# class UnivCertView(views.APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def post(self, request):
-#         """
-#         학교 이메일과 대학명을 받아 인증을 처리합니다.
-#         (현재는 테스트를 위해 입력값만 있으면 무조건 성공 처리)
-#         """
-#         email = request.data.get('email')
-#         univ_name = request.data.get('univ_name')
-        
-#         # [TODO] 실제 서비스 배포 시 아래 주석을 해제하고 Univcert API Key를 .env에서 가져오세요.
-#         """
-#         import os
-#         API_KEY = os.getenv("UNIVCERT_API_KEY")
-#         response = requests.post(
-#             'https://univcert.com/api/v1/certify',
-#             json={"key": API_KEY, "email": email, "univName": univ_name, "univ_check": True}
-#         )
-#         if not response.json().get('success'):
-#              return Response({"error": "인증 메일 발송 실패"}, status=400)
-#         """
-        
-#         # [TEST 모드] 입력값이 있으면 인증 성공으로 처리 (개발용)
-#         if email and univ_name:
-#             user = request.user
-#             user.univ_email = email
-#             user.is_student_verified = True # 바로 인증 완료 처리
-#             user.save()
-            
-#             return Response({
-#                 "message": "인증이 완료되었습니다. (테스트 모드)",
-#                 "user_id": user.id,
-#                 "is_verified": user.is_student_verified
-#             }, status=status.HTTP_200_OK)
-            
-#         return Response({"error": "이메일과 학교명을 정확히 입력해주세요."}, status=status.HTTP_400_BAD_REQUEST)
-=======
 #로그인 페이지
 def login_page(request):
     return render(request,'users/login.html')
@@ -197,4 +157,3 @@ class MyLoginView(APIView):
         
 def logout(request):
     return render(request,'users/logout.html')
->>>>>>> b6e9111ff188318252829ec8f8a9c38a8c14dbaf
